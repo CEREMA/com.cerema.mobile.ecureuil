d4dd0c64d25918d78eea25c176781089c420e855
Stephane Zucatti <stephane.zucatti@me.com>
2015-03-12 15:26:21 +0100

    Update# 24

--- src/Contents/Application/app/view/main/VListAO.js
+++ src/Contents/Application/app/view/main/VListAO.js
@@ -6,7 +6,27 @@ App.view.define('main.VListAO', {
 	config: {
 		title: "Appel d'offres",
 		items: [
-	
+			{
+				xtype: "list",
+				store: App.store.create({
+					fields: [
+						"firstName",
+						"lastName"
+					],
+					data: [
+						{
+							firstName: "Stephane",
+							lastName: "Zucatti"
+						}
+					]
+				}),
+				itemTpl: '<div class="contact">{firstName} {lastName}</div>',
+				grouped: true,
+				onItemDisclosure: function(record, btn, index) {
+					ListDemo.detailPanel.update(record.data);
+					ListDemo.Viewport.setActiveItem('detailpanel');
+				}				
+			}
 		]
 	}
 });
